d3207e138fd7ffb9d8b284f77a0faa84ada9cdbe
Daniel Abrao <danielpaladar@gmail.com>
2019-08-09 17:23:05 -0300

    Update test code

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ import os
 import json
 import pytest
 
-import __init__
+import main.__init__ as __init__
 
 SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
